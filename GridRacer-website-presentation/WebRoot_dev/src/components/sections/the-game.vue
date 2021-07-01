b2cc836cbb7dc2e1665020b61afb3c3646d63a16
Thomas G.S <somerandomcodes@gmail.com>
2021-07-02 01:01:14 +0200

    Projekt fertig

--- GridRacer-website-presentation/WebRoot_dev/src/components/sections/the-game.vue
+++ GridRacer-website-presentation/WebRoot_dev/src/components/sections/the-game.vue
@@ -23,7 +23,7 @@
           </p>
         </div>
         <div class="col-6">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/Qa1riAP9rFM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/3rb0Ld1cdGw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </div>
       </div>
     </div>
   </section>
